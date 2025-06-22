6771ef83346e3019e14ce363462d157a04ddddf5
Jannatul Faria <kairasana8@gmail.com>
2025-06-22 23:03:25 +0600

    another fb-page-plugin iframe test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,6 +68,15 @@
                        
                         allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
                     </iframe>
+                    {{-- <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FXtremePaintballTN&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                     width="500" 
+                     height="500" 
+                     style="border:none;overflow:hidden" 
+                     scrolling="no" 
+                     frameborder="0" 
+                     allowfullscreen="true" 
+                     allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+                    </iframe> --}}
                 </div>
 
             </div>
